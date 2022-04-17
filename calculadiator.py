a35644ecce52c00bfc1344d72616b0ad92be184e
Hjortur_ <97688098+hjorturhelgi@users.noreply.github.com>
2022-04-17 23:00:32 +0000

    Update calculadiator.py
    Translated from icelandic to english

--- calculadiator.py
+++ calculadiator.py
@@ -2,113 +2,113 @@
 
 import random
 
-def plús(a, b):
-    s = svar()
+def plus(a, b):
+    s = answer()
     resault = a + b
     if s == resault:
-        print(f"Vel gert rétta svarið var: {resault}")
+        print(f"Well done, {resault} is the right answer.")
         return True
     else:
-        print(f"Rangt svar! Rétt svar var: {resault}")
+        print(f"Sorry, the right answer was {resault}.")
         return False
 
-def mínus(a, b):
-    s = svar()
+def minus(a, b):
+    s = answer()
     resault = a - b
     if s == resault:
-        print(f"Vel gert rétta svarið var: {resault}")
+        print(f"Well done, {resault} is the right answer.")
         return True
     else:
-        print(f"Rangt svar! Rétt svar var: {resault}")
+        print(f"Sorry, the right answer was {resault}.")
         return False
 
-def deiling(a, b):
-    s = svar()
+def division(a, b):
+    s = answer()
     resault = a // b
     if s == resault:
-        print(f"Vel gert rétta svarið var: {resault}")
+        print(f"Well done, {resault} is the right answer.")
         return True
     else:
-        print(f"Rangt svar! Rétt svar var: {resault}")
+        print(f"Sorry, the right answer was {resault}.")
         return False
 
-def margföldun(a, b):
-    s = svar()
+def multiplication(a, b):
+    s = answer()
     resault = a * b
     if s == resault:
-        print(f"Vel gert rétta svarið var: {resault}")
+        print(f"Well done, {resault} is the right answer.")
         return True
     else:
-        print(f"Rangt svar! Rétt svar var: {resault}")
+        print(f"Sorry, the right answer was {resault}.")
         return False
 def svar():
     return int(input("Svar: "))
 
 def main():
-    teljari = 1
+    counter = 1
     x = 0
     z = 0
-    while teljari < 11:
-        print(f"Dæmi {teljari}")
-        if byrja == "+":
+    while counter < 11:
+        print(f"Dæmi {counter}")
+        if start == "+":
             t1 = 1
             t2 = 100
             t3 = 1
             t4 = 100
-            tala_1 = random.randint(t1, t2)
-            tala_2 = random.randint(t3, t4)
-            print(f"Hvað er {tala_1} plús {tala_2}")
-            samasem = plús(tala_1, tala_2)
-        elif byrja == "-":
+            number_1 = random.randint(t1, t2)
+            number_2 = random.randint(t3, t4)
+            print(f"What is {number_1} plus {number_2}")
+            equals = plus(number_1, number_2)
+        elif start == "-":
             t1 = 50
             t2 = 100
             t3 = 1
             t4 = 50
-            tala_1 = random.randint(t1, t2)
-            tala_2 = random.randint(t3, t4)
-            print(f"Hvað er {tala_1} mínus {tala_2}")
-            samasem = mínus(tala_1, tala_2)
-        elif byrja == "*":
+            number_1 = random.randint(t1, t2)
+            number_2 = random.randint(t3, t4)
+            print(f"What is {number_1} minus {number_2}")
+            equals = minus(number_1, number_2)
+        elif number == "*":
             t1 = 1
             t2 = 10
             t3 = 1
             t4 = 11
-            tala_1 = random.randint(t1, t2)
-            tala_2 = random.randint(t3, t4)
-            print(f"Hvað er {tala_1} sinnum {tala_2}")
-            samasem = margföldun(tala_1, tala_2)
-        elif byrja == "/":
+            number_1 = random.randint(t1, t2)
+            number_2 = random.randint(t3, t4)
+            print(f"What is {number_1} times {number_2}")
+            equals = multiplication(number_1, number_2)
+        elif start == "/":
             q = 1
             while q != 0:
                 t1 = 4
                 t2 = 50
                 t3 = 2
                 t4 = 4
-                tala_1 = random.randint(t1, t2)
-                tala_2 = random.randint(t3, t4)
-                q = tala_1 % tala_2
+                number_1 = random.randint(t1, t2)
+                number_2 = random.randint(t3, t4)
+                q = number_1 % number_2
                 if q == 0:
-                    print(f"Hvað er {tala_1} deilt með {tala_2}")
-                    samasem = deiling(tala_1, tala_2)
+                    print(f"What is {number_1} divided by {number_2}")
+                    equals = division(number_1, number_2)
         else:
             print("OKAY")
-        if samasem:
+        if equals:
             x += 1
         else:
             z += 1
-        teljari += 1
-        print(f"Rétt svör {x}\nRöng svör {z}")
+        counter += 1
+        print(f"Right answers {x}\nWrong answers {z}")
     if z == 0:
-        print(f"TIL HAMINGJU! VEL GERT! Þú ert með {x} í einkunn.")
+        print(f"Congratulations! Your score is {x}.")
     elif x == 0:
-        print(f"Heyrðu! Þú þarft að taka þig á og reikna meira og oftar! Einkunnin þín er {x}.")
+        print(f"You could do with a bit more practice, you scored {x}.")
     else:
-        print(f"Þú ert með {x} í einkunn. Þú varst dreginn niður um {z} vegna rangra svara.")
+        print(f"Your score is {x}. You had {z} wrong answers.")
         
-# while byrja == True: # 1.skref - val notanda
+# while start == True: # 1.step - users pick
 #    try:    
-byrja = input("Hæ hæ velkomin í stærðfræði. Ýttu á + - * eða / til að byrja.")
-if byrja == "+" or "-" or "*" or "/":
+start = input("Hi there and welcome to.....math... Push eather + - * or / to start.")
+if start == "+" or "-" or "*" or "/":
     main()
 #    except:
-#        print("Innsláttarvilla! Þú verður að velja + eða - eða * eða / til að halda áfram.")
+#        print("You have to choose eather + or - or * or / to begin.")
